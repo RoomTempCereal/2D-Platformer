b128a845063b2a7df33014a5065d5d88a4abbaf8
RoomTempCereal <79651217+RoomTempCereal@users.noreply.github.com>
2022-12-02 11:07:27 +0000

    left, right and jump movement

--- 2D Platformer/Assets/PlayerMovement.cs
+++ 2D Platformer/Assets/PlayerMovement.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
